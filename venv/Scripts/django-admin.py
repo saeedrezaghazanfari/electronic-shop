77ea7ead3f9603fcdeea17c082688663a45bf8d5
سعیدرضا غضنفری <saeedreza.gh.1397@gmail.com>
2020-11-04 23:39:29 +0330

    commited after change .gitignore file

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!C:\Users\SaaedReza\Desktop\P.E.A.K.A. H.R.C\Command_PEAKA\__BackEnd_Django_Project__\Electronic__Shop\venv\Scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
